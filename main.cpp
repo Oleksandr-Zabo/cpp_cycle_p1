8af313f9fa87a33b785f8166441f2ec4b33514a1
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-02 19:13:01 +0300

    ex. 4 lab had done

--- main.cpp
+++ main.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 int main()
-{//dev
-	cout << "Hello world" << endl;
+{
+	int num_in, sum=0;
+	
+	do {
+		cout << "Enter your number: ";
+		cin >> num_in;
+		sum += num_in;
+	} while (num_in != 0);
+
+	cout << "Sum of numbers: " << sum << endl;
 	system("pause");
 	return 0;
 }
