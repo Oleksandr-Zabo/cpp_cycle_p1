36d69f09d0db28e776188e61fc707c534575b034
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-02 20:07:20 +0300

    ex.5 hw had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
+#include <windows.h> 
 
 int main()
-{//dev
-	cout << "Hello world" << endl;
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	
+	int k_in, i=1;
+	cout << "Enter your number: ";
+	cin >> k_in;
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	while(i <= 10) {
+		cout <<k_in<< " x " << i
+			<< " = " << k_in*i
+			<< endl;
+		i++;
+	}
+	SetConsoleTextAttribute(hConsole, 0x07);
+	
 	system("pause");
 	return 0;
 }
