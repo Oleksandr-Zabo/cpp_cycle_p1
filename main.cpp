2754328c4d4443c2c41e1f4051aab3568b63ea1a
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-02 19:08:32 +0300

    ex. 3 lab had done

--- main.cpp
+++ main.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main()
-{//dev
-	cout << "Hello world" << endl;
+{
+	int num_start, num_end, sum=0;
+	cout << "Enter the beginning of the range: " << endl;
+	cin >> num_start;
+	cout << "Enter the end of the range: " << endl;
+	cin >> num_end;
+
+	if (num_start > num_end) {
+
+		while (num_start >= num_end) {
+			sum += num_end++;
+		}
+	}
+	else {
+
+		while (num_start <= num_end) {
+			sum += num_start++;
+		}
+	}
+	cout << "The sum of the range: " << sum << endl;
 	system("pause");
 	return 0;
 }
