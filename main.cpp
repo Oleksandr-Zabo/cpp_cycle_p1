6354ac5695667d5b74b68eee5e92e9222436f4a1
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-02 18:28:56 +0300

    ex.1 lab had done

--- main.cpp
+++ main.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main()
-{//dev
-	cout << "Hello world" << endl;
+{
+	int number_in, num_i = 0;
+	cout << "Enter your number: ";
+	cin >> number_in;
+	if (number_in > 0) {
+		while (number_in + 1 != num_i) {
+			cout << num_i++ << endl;
+		}
+	}
+	else if ((number_in < num_i)) {
+		while (number_in - 1 != num_i) {
+			cout << num_i-- << endl;
+		}
+	}
+	else {
+		cout << 0 << endl;
+	}
+
 	system("pause");
 	return 0;
 }
