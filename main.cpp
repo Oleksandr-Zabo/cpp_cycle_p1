270685a1f78a9b14cb388509e98deb7915c50889
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-02 19:33:45 +0300

    ex. 1 hw had done

--- main.cpp
+++ main.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main()
-{//dev
-	cout << "Hello world" << endl;
+{//From a to 500 inclusive
+	int a_in, sum=0;
+	cout << "Enter your number: ";
+	cin >> a_in;
+	if (a_in <= 500) {
+		while (a_in <= 500)
+			sum += a_in++;
+	}
+	else{
+		while (a_in >= 500)
+			sum += a_in--;
+	}
+	cout << "Sum of numbers: " << sum << endl;
 	system("pause");
 	return 0;
 }
