74be7261af4cf5a3fe7074f9c367d418679e974a
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-02 19:57:49 +0300

    ex. 4 hw had done

--- main.cpp
+++ main.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main()
-{//dev
-	cout << "Hello world" << endl;
+{
+	int a_in;
+	long long product = 1;
+	cout << "Enter your number (1-19): ";
+	cin >> a_in;
+	do {
+
+	} while(a_in < 0 || a_in > 20);
+	while (a_in <=20) {
+		product *= a_in++;
+	}
+	cout << "Product of numbers: "<< product << endl;
 	system("pause");
 	return 0;
 }
