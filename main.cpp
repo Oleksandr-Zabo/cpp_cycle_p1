4be152e6fa84d59d268f75f615fc6bcac2662c4a
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-02 20:51:44 +0300

    ex. 1 add work had done

--- main.cpp
+++ main.cpp
@@ -2,8 +2,34 @@
 using namespace std;
 
 int main()
-{//dev
-	cout << "Hello world" << endl;
+{
+	int lenth, i=0;
+	bool vertical_horisontal;
+	char char_in;
+	cout << "Enter lenth of line: ";
+	cin >> lenth;
+	cout << "Enter vertical(0) or horisontal line(1): ";
+	cin >> vertical_horisontal;
+	cout << "Enter char: ";
+	cin >> char_in;
+	switch (vertical_horisontal)
+	{
+	case 0: {
+		while (i < lenth) {
+			cout << char_in;
+			i++;
+		}
+	}
+
+	case 1: {
+		while (i < lenth) {
+			cout << char_in << endl;
+			i++;
+		}
+	}
+	}
+
+	cout<< endl;
 	system("pause");
 	return 0;
 }
