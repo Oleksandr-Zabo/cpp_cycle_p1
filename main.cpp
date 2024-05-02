62fd5017afbd329e65cf00d7006af77a52af794d
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-02 20:01:14 +0300

    ex. 4 hw had done  (add a=20)

--- main.cpp
+++ main.cpp
@@ -5,14 +5,17 @@ int main()
 {
 	int a_in;
 	long long product = 1;
-	cout << "Enter your number (1-19): ";
+	cout << "Enter your number (1-20): ";
 	cin >> a_in;
 	do {
 
 	} while(a_in < 0 || a_in > 20);
-	while (a_in <=20) {
-		product *= a_in++;
+	if (a_in < 20) {
+		while (a_in <= 20) {
+			product *= a_in++;
+		}
 	}
+	else product = 20 * 20;
 	cout << "Product of numbers: "<< product << endl;
 	system("pause");
 	return 0;
